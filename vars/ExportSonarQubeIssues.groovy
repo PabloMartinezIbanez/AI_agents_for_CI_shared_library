708a75cbcfce8158d0a28e567718e23a04cc639a
PabloMartinezIbanez <pablo.martinezibanez@edu.turkuamk.fi>
2026-03-15 13:57:46 +0200

    Update ExportSonarQubeIssues to install Python dependencies before running the script

--- vars/ExportSonarQubeIssues.groovy
+++ vars/ExportSonarQubeIssues.groovy
@@ -1,7 +1,10 @@
 def call() {
     script {
         def exportScript = libraryResource 'export_sonarqube_issues.py'
+        def requirements = libraryResource 'requirements.txt'
         writeFile file: 'export_sonarqube_issues.py', text: exportScript
+        writeFile file: 'requirements.txt', text: requirements
+        sh 'pip3 install -r requirements.txt'
         sh 'python3 export_sonarqube_issues.py'
     }
 }
