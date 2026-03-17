919ef55b26af7d72bb7accdf7ee2833301a2f654
PabloMartinezIbanez <pablomartinezibanez04@gmail.com>
2026-03-17 20:12:01 +0200

    Changed requirements site in workspace

--- vars/ExportSonarQubeIssues.groovy
+++ vars/ExportSonarQubeIssues.groovy
@@ -3,11 +3,11 @@ def call() {
         def exportScript = libraryResource 'export_sonarqube_issues.py'
         def requirements = libraryResource 'requirements.txt'
         writeFile file: 'export_sonarqube_issues.py', text: exportScript
-        writeFile file: 'requirements.txt', text: requirements
+        writeFile file: '/lib_requirements/requirements.txt', text: requirements
         sh '''
             python3 -m venv venv
             . venv/bin/activate
-            pip install -r requirements.txt
+            pip install -r /lib_requirements/requirements.txt
             python export_sonarqube_issues.py > /dev/null 2>&1
         '''
     }
